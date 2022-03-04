fec5344e4f9c2fb7d09397a47b9e556f11c65d12
Jovana Mitrovic <49660657+mitrovicjovana@users.noreply.github.com>
2022-03-04 23:20:09 +0100

    Add User entity

--- src/main/java/com/example/agileboardsserver/config/SecurityConfig.java
+++ src/main/java/com/example/agileboardsserver/config/SecurityConfig.java
@@ -1,11 +1,8 @@
 package com.example.agileboardsserver.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -20,10 +17,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest()
                         .permitAll());
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
     }
 }
