09b6db3567a96bfe8ac73e37fb3c1f511de3087e
Jovana Mitrovic <49660657+mitrovicjovana@users.noreply.github.com>
2022-03-04 18:26:54 +0100

    Initial commit, create Spring Boot application

---
+++ src/main/java/com/example/agileboardsserver/AgileBoardsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.agileboardsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileBoardsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgileBoardsServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/agileboardsserver/AgileBoardsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agileboardsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileBoardsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
